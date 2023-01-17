ec5d8000bb4c20fbff65b3e325e297f8cccdec24
Maria <mariamar_@hotmail.gr>
2023-01-17 13:54:26 +0000

    updated state using setState method

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -1,9 +1,28 @@
 import React from "react";
 
 class StatefulGreeting extends React.Component {
-     render(){
-        return <h1>Hello {this.props.name}, {this.props.greeting}</h1>
-     }
+   constructor(props) {
+      super(props);
+      this.state = {
+         introduction: "Hello",
+         buttonText: "Exit",
+      };
+   }
+
+   handleClick() {
+      this.setState({
+         introduction: "Goodbye",
+      });
+      console.log()
+   }
+   render(){
+      return (
+         <div>
+            <h1>{this.state.introduction} {this.props.name}, {this.props.greeting}</h1>
+            <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+         </div>
+      )
+   }
 }
 
 export default StatefulGreeting
